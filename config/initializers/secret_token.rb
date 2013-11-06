2f2273bebb1171f484c68a0d633e771d267f7795
Jason Terhorst <yourstruly@jterhorst.com>
2013-11-05 20:41:19 -0600

    App setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Richwoodshashtag::Application.config.secret_token = 'a1c1f853fe0550906736a54f637fbd1db68e219ff1d2901b61961ca3df4cc2fdd4f785b7d710e96677d739438ffa8f5337e376b3dd1e36258ba4a3fcfff28a26'
